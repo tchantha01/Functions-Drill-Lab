f3b6cba7485998249224a67db9a8579180977596
Toky Chanthavong <tchantha01@gmail.com>
2022-08-22 16:50:05 -0600

    functions lab

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -199,20 +199,20 @@ console.log(nameGreeting)
 
 //CODE HERE
 
-function faveColorFinder(color){
-  if(color === 'red'){
-    return 'red is a great color'
-  }else if(color === 'green'){
-    return 'green is a solid favorite color'
-  }else if(color === 'black'){
-    return 'so trendy'
-  }else {
-    return 'you need to evaluate your favorite color choice'
-  }
-}
+// function faveColorFinder(color){
+//   if(color === 'red'){
+//     return 'red is a great color'
+//   }else if(color === 'green'){
+//     return 'green is a solid favorite color'
+//   }else if(color === 'black'){
+//     return 'so trendy'
+//   }else {
+//     return 'you need to evaluate your favorite color choice'
+//   }
+// }
 
-let colorRating = faveColorFinder('black')
-console.log(colorRating)
+// let colorRating = faveColorFinder('blue')
+// console.log(colorRating)
 
 ////////////////// PROBLEM 12 ////////////////////
 let namesArr = ['Cameron', 'Riley', 'Eric', 'Brenna', 'Karl']
